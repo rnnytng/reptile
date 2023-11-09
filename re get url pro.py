19d041d7ab7c28e97a3b99435ae2e12100b811d3
YIT Ren <145679191+rnnytng@users.noreply.github.com>
2023-11-09 16:37:42 +0800

    Add files via upload

---
+++ re get url pro.py
@@ -0,0 +1,11 @@
+import requests
+import re
+page=input("输入类别:")#小写拼音
+url=f"https://www.4kdesk.com/4K{page}/"
+response=requests.get(url)
+response.encoding='utf-8'
+obj=re.compile(r'<a href=(?P<urll>.*?)target="_blank" title=(?P<name>.*?)\d.*?>')
+result=obj.finditer(response.text)
+for item in result:
+    dict=item.groupdict()
+    print(dict)
